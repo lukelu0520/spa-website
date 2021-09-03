9e964c50419734d5d9fc0e1402b4b3e5fd3763d7
lukelu0520 <appl.chlu@gmail.com>
2021-09-03 15:58:44 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Hard Workers are Internal Testing
+        </p>
+        <p>
+           WECHAT: LUKELU
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/lukelu520"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Linkedin
         </a>
       </header>
     </div>
